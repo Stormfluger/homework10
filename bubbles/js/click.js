54eb5927eb1fd6140c98dce24b962df69f33eb4c
Stormfluger <turkom76@mail.ru>
2018-05-07 18:29:05 +0500

    work

--- bubbles/js/click.js
+++ bubbles/js/click.js
@@ -1,1 +1,15 @@
+
 'use strinct';
+
+const socketConnect = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+socketConnect.addEventListener('open', (event) => {
+  showBubbles(event.target);
+});
+
+document.addEventListener('click', (event) => {
+  socketConnect.send(JSON.stringify({
+    x: event.clientX,
+    y: event.clientY
+  }));
+});
--- counter/js/counter.js
+++ counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const socket = new WebSocket('wss://neto-api.herokuapp.com/counter'),
+      connections = document.querySelector('.counter'),
+      errors = document.querySelector('output.errors');
+
+window.addEventListener('beforeunload', () => {
+  socket.close(1000, 'Соединение закрыто');
+});
+
+socket.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
+  connections.textContent = data.connections;
+  errors.textContent = data.errors;
+});
